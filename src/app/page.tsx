15ad06e1de8435b79b8a8d52087d4c74b9bea40f
anguo liu <angusliu9898@gmail.com>
2025-08-12 06:40:49 +0000

    Make the following changes:
    On the page src/app/page.tsx,
    
    Find this el

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -377,7 +377,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-full bg-background text-foreground">
       <AppHeader userRole={userRole} onLogout={logout} />
-      <main className="flex-1 overflow-hidden p-4 md:p-6 lg:p-8">
+      <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
         {renderContent()}
       </main>
     </div>
